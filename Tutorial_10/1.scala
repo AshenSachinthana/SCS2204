8bf56f925f67b763bc2599c0b01b0745721a59f6
AshenSachinthana <136717556+AshenSachinthana@users.noreply.github.com>
2023-08-29 18:34:54 +0530

    Tutorial_10

--- Tutorial_10/1.scala
+++ Tutorial_10/1.scala
@@ -1,1 +1,17 @@
+object Q1{
+    def main(args: Array[String]): Unit = {
+        val celsiusTemp: List[Double] = List(0, 10, 20, 30)
+        val averageFahrenheit = calculateAverage(celsiusTemp)
+        println(s"Average Fahrenheit : $averageFahrenheit")
+    }
 
+    def calculateAverage(celsiusTemp: List[Double]): Double = {
+        val fahrenheitTemp: List[Double] = celsiusTemp.map(c => (c * 9 / 5) + 32)
+
+        val sum: Double = fahrenheitTemp.reduce((a, b) => a + b)
+
+        val averageFahrenheit: Double = sum / fahrenheitTemp.length
+        averageFahrenheit
+    }
+}
+ 
